77c0b3a827bf58184ac4ea26bb59a113dd94f45c
dead-like-roses <etooilikespizza@gmail.com>
2024-07-21 09:05:11 +0200

    feat: activity service and handler

--- handlers/activity.handler.go
+++ handlers/activity.handler.go
@@ -2,17 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/dead-like-roses/od_tracker/services"
 	"github.com/labstack/echo/v4"
 )
+
+type ActivityRequest struct {
+	Device   string    `json:"device"`
+	PostedAt time.Time `json:"posted_at"`
+}
 
 type ActivityHandler struct {
 	service services.ActivityService
 }
 
 func (ah ActivityHandler) registerActivity(c echo.Context) error {
-	return c.String(http.StatusCreated, "registered datapoint")
+	a := new(ActivityRequest)
+	if err := c.Bind(a); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, a)
 }
 
 func (ah ActivityHandler) listDatapoints(c echo.Context) error {
--- services/activity.service.go
+++ services/activity.service.go
@@ -3,6 +3,9 @@ package services
 import (
 	"database/sql"
 	"time"
+
+	"github.com/dead-like-roses/od_tracker/.gen/od_track/public/model"
+	"github.com/dead-like-roses/od_tracker/.gen/od_track/public/table"
 )
 
 func NewActivityService(db *sql.DB) *ActivityService {
@@ -16,4 +19,18 @@ type ActivityService struct {
 }
 
 func (as ActivityService) createDataPoint(device string, posted time.Time) {
+	data := model.Activity{DeviceID: device, PostedAt: posted, App: "none"}
+
+	insertStmt := table.Activity.INSERT(table.Activity.DeviceID, table.Activity.PostedAt, table.Activity.App).MODEL(data)
+	insertStmt.Exec(as.db)
+}
+
+func (as ActivityService) createMultipleDataPoints(device string, times []time.Time) {
+	dataPoints := make([]model.Activity, len(times))
+	for i, t := range times {
+		dataPoints[i] = model.Activity{DeviceID: device, PostedAt: t, App: "none"}
+	}
+
+	insertStmt := table.Activity.INSERT(table.Activity.DeviceID, table.Activity.PostedAt, table.Activity.App).MODELS(dataPoints)
+	insertStmt.Exec(as.db)
 }
